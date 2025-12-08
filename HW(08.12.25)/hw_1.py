0c3b4c5c60b1941f7589e50895c915c2a2bca17e
grinles <157224367+grinles2@users.noreply.github.com>
2025-12-08 18:17:45 +0000

    Add files via upload

---
+++ HW(08.12.25)/hw_1.py
@@ -0,0 +1,16 @@
+
+hw_ans = input("Номер Задания? 1-3 --->")
+if hw_ans == "1":
+    print("-----------------------------------------------")
+    print("To be\nor not\nto be")
+if hw_ans == "2":
+    print("-----------------------------------------------")
+    print("  'Life is what happens \n \t when \n\t you're busy making other plans'\n \t John Lennon  ")
+if hw_ans == "3":
+    print("-----------------------------------------------")
+    print(' "The best way to predict the future\n\tis to\n\t\tinvent it \n\t\t\tAlan Kay"  ')
+else:
+    exit()
+
+
+
