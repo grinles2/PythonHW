7db8b7d669161fa410607b6c4dc7c1bd9edd81ad
grinles <157224367+grinles2@users.noreply.github.com>
2025-12-22 17:11:42 +0000

    Add files via upload

---
+++ ClassWork/07-12-25.py
@@ -0,0 +1,19 @@
+print("Hello world!")
+print('Hello world 2')
+
+print(14, 15, 25, 56)
+print(14, "text", 78)
+
+print(13, 12, 45, 34, sep=', ')
+
+print("The game of the year is 'Clair Obscur: Expedition 33'")
+print('The game of the year is "Clair Obscur: Expedition 33"')
+print("The game of the year is \"Clair Obscur: Expedition 33\"")
+print('The game of the year is \'Clair Obscur: Expedition 33\'')
+print("\\ - символ з якого починаються escape-послідовності")
+
+print("Цей рядок розбитий\nна два рядки")
+print("\t цей текст написаний з абзацу (табуляція)")
+
+print("Цей прінт і наступний", end=" ")
+print("На одному рядку")
