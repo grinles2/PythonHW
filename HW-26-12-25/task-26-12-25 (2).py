571a2a13912e0e6ff25ccf843c63ac64a737728f
grinles <157224367+grinles2@users.noreply.github.com>
2025-12-27 10:58:59 +0000

    Add files via upload

---
+++ HW-26-12-25/task-26-12-25 (2).py
@@ -0,0 +1,12 @@
+import time
+num1 = float( input("Диагональ 1 ромба: ") )
+num2 = float( input("Диагональ 2 ромба: ") )
+print("Обработка...")
+
+ans = (num1 * num2)
+
+ans = ( ans / 2 )
+
+time.sleep(1)
+
+print(ans, "^2 единиц")
---
+++ HW-26-12-25/task_26-12-25 ( 1 ).py
@@ -0,0 +1,9 @@
+import time
+num1 = float( input("Введите ваш возвраст: ") )
+num2 = float( input("Введите кол.во смертей в кс-2: ") )
+print("Обработка...")
+time.sleep(1)
+ans = (num1 - num2)
+ans2 = (num1 + num2)
+print(ans, "разница чисел")
+print(ans2, "сумма чисел")
